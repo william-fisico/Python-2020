19738d38213219f948cf0844762308fd16b4f2f8
William Santos <william.wapsantos@gmail.com>
2020-05-01 21:55:14 -0300

    Initial

---
+++ hello.py
@@ -0,0 +1,26 @@
+# Clear the terminal screen
+import os
+os.system('clear')
+
+#############################
+# DATA TYPES
+# Strings
+# Numbers
+# Lists
+# Tuples
+# Dictionaries
+# Boolean
+############################
+
+
+
+first_name = 'William'
+age = 35
+fav_pizza = {
+	"John": "Pepperoni",
+	"Bob": "Mushroom",
+	"Mary": "Cheese"	
+	}
+name = True
+
+print (name)
