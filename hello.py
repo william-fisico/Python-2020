1f550bb4ccfb2d94aa37a344eb6133d11eb65e92
William Santos <william.wapsantos@gmail.com>
2020-05-01 22:00:54 -0300

    Add some coment for test

--- hello.py
+++ hello.py
@@ -1,17 +1,23 @@
+###########################################
+# William A. P. dos Santos                #
+# william.wapsantos@gmail.com             #
+# Testanto Git-Hub                        #
+# 01 de maio de 2020                      #
+###########################################
+
 # Clear the terminal screen
 import os
 os.system('clear')
 
-#############################
-# DATA TYPES
-# Strings
-# Numbers
-# Lists
-# Tuples
-# Dictionaries
-# Boolean
-############################
-
+'''
+	DATA TYPES
+	Strings
+	Numbers
+	Lists
+	Tuples
+	Dictionaries
+	Boolean
+'''
 
 
 first_name = 'William'
