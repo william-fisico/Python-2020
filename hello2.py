43457654eb199ed3eb537226571223528a216eb7
William Santos - Notebook Samsung <william.wapsantos@gmail.com>
2020-05-11 20:16:35 -0300

    Add hello2.py

---
+++ hello2.py
@@ -0,0 +1,33 @@
+###########################################
+# William A. P. dos Santos                #
+# william.wapsantos@gmail.com             #
+# Testanto Git-Hub                        #
+# 01 de maio de 2020                      #
+###########################################
+
+# Clear the terminal screen
+import os
+os.system('clear')
+
+'''
+	DATA TYPES
+	Strings
+	Numbers
+	Lists
+	Tuples
+	Dictionaries
+	Boolean
+'''
+
+
+first_name = 'William'
+last_name = 'Alves Pinto dos Santos'
+age = 35
+fav_pizza = {
+	"John": "Pepperoni",
+	"Bob": "Mushroom",
+	"Mary": "Cheese"	
+	}
+name = first_name + " " + last_name
+
+print (name)
